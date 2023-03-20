f2abfa8ca9412b05909ee265aacbaf9b7592b032
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-03-20 14:22:52 +0100

    Eigth test (8) - Red

--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsShould {
     @ParameterizedTest
-    @CsvSource({"1, I", "2, II", "3, III", "4, IV", "5, V", "6, VI", "7, VII"})
+    @CsvSource({"1, I", "2, II", "3, III", "4, IV", "5, V", "6, VI", "7, VII", "8, VIII"})
     void onesToRomanNumerals(int input, String expectedOutput) {
        assertEquals(expectedOutput, new RomanNumerals().convert(input));
     }
