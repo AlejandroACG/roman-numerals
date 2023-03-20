21e382e97d2748daadc704f4c65e3b532a70b43c
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-03-20 16:24:22 +0100

    16th test (60) - Green

--- src/main/java/es/sanvalero/entornos/RomanNumerals.java
+++ src/main/java/es/sanvalero/entornos/RomanNumerals.java
@@ -56,6 +56,8 @@ public class RomanNumerals {
             romanTens = "XL";
         } else if (tens == 5) {
             romanTens = "L";
+        } else if (tens == 6) {
+            romanTens = "LX";
         }
 
         return romanTens;
