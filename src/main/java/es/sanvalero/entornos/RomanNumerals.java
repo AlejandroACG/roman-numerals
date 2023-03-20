6fb9e4f1373521e3e32c14516428537d408aa033
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-03-20 14:03:41 +0100

    First test (1) - Green

--- src/main/java/es/sanvalero/entornos/RomanNumerals.java
+++ src/main/java/es/sanvalero/entornos/RomanNumerals.java
@@ -3,6 +3,10 @@ package es.sanvalero.entornos;
 public class RomanNumerals {
     public static String convert (int number) {
         String romanNumeral = null;
+        
+        if (number == 1) {
+            romanNumeral = "I";
+        }
 
         return romanNumeral;
     }
--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsShould {
     @Test
-    void romanNumeralsConvert() {
+    void romanNumeralsConvert_1() {
         assertEquals("I", new RomanNumerals().convert(1));
     }
 }
