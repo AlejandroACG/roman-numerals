1822ee8abff8511a1c238fe7fe3989430f7f6141
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-03-20 15:28:50 +0100

    Refactoring
    - Simplified the class Main
    - Refactored the class RomanNumerals to start preparing it for digits larger than 10 and started using functions to make the code clearer

--- src/main/java/es/sanvalero/entornos/Main.java
+++ src/main/java/es/sanvalero/entornos/Main.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        boolean exit = false;
-        do {
-            System.out.print("Number (from 1 to 3000) to convert into roman numerals: ");
-            int number = keyboard.nextInt();
-            System.out.println(RomanNumerals.convert(number));
-            System.out.print("Do you want to convert another number? (Y/N) ");
-            String exitString = keyboard.nextLine();
-            if (!exitString.equalsIgnoreCase("Y")) { exit = true; }
-        } while (!exit);
+
+        System.out.print("Number (from 1 to 3000) to convert into roman numerals: ");
+        int number = keyboard.nextInt();
+        System.out.println(RomanNumerals.convert(number));
     }
 }
--- src/main/java/es/sanvalero/entornos/RomanNumerals.java
+++ src/main/java/es/sanvalero/entornos/RomanNumerals.java
@@ -2,36 +2,38 @@ package es.sanvalero.entornos;
 
 public class RomanNumerals {
     public static String convert (int number) {
+        String romanOnes = null;
         String romanNumeral = null;
-        
-        if (number == 1) {
-            romanNumeral = "I";
-        }
-        if (number == 2) {
-            romanNumeral = "II";
-        }
-        if (number == 3) {
-            romanNumeral = "III";
-        }
-        if (number == 4) {
-            romanNumeral = "IV";
-        }
-        if (number == 5) {
-            romanNumeral = "V";
-        }
-        if (number == 6) {
-            romanNumeral = "VI";
-        }
-        if (number == 7) {
-            romanNumeral = "VII";
-        }
-        if (number == 8) {
-            romanNumeral = "VIII";
-        }
-        if (number == 9) {
-            romanNumeral = "IX";
+
+        if (number <= 10) {
+            romanOnes = onesConversion(number);
+            romanNumeral = romanOnes;
         }
 
         return romanNumeral;
+    }
+
+    private static String onesConversion(int ones) {
+        String romanOnes;
+        if (ones == 1) {
+            romanOnes = "I";
+        } else if (ones == 2) {
+            romanOnes = "II";
+        } else if (ones == 3) {
+            romanOnes = "III";
+        } else if (ones == 4) {
+            romanOnes = "IV";
+        } else if (ones == 5) {
+            romanOnes = "V";
+        } else if (ones == 6) {
+            romanOnes = "VI";
+        } else if (ones == 7) {
+            romanOnes = "VII";
+        } else if (ones == 8) {
+            romanOnes = "VIII";
+        } else {
+            romanOnes = "IX";
+        }
+        return romanOnes;
     }
 }
