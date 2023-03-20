94956a3a4f4ef274a2f745b6a4aa65a1aaa0eedb
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-03-20 19:00:12 +0100

    32th test (1100) - Red

--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -22,7 +22,7 @@ public class RomanNumeralsShould {
         assertEquals(expectedOutput, new RomanNumerals().convert(input));
     }
     @ParameterizedTest
-    @CsvSource({"1000, M"})
+    @CsvSource({"1000, M", "1100, MC"})
     void thousandsToRomanNumerals(int input, String expectedOutput) {
         assertEquals(expectedOutput, new RomanNumerals().convert(input));
     }
