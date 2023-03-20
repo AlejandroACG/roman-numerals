ba393ddf3a8f1183b5d2c42cbb102dd412c622fe
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-03-20 14:21:42 +0100

    Seventh test (7) - Red

--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsShould {
     @ParameterizedTest
-    @CsvSource({"1, I", "2, II", "3, III", "4, IV", "5, V", "6, VI"})
+    @CsvSource({"1, I", "2, II", "3, III", "4, IV", "5, V", "6, VI", "7, VII"})
     void onesToRomanNumerals(int input, String expectedOutput) {
        assertEquals(expectedOutput, new RomanNumerals().convert(input));
     }
