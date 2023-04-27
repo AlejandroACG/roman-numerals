058473e71400e9c963982cb4a5b2738fc213ef61
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-04-27 15:26:47 +0200

    Test renamed

--- src/test/java/RomanNumeralsShouldTest.java
+++ src/test/java/RomanNumeralsShouldTest.java
@@ -1,11 +1,10 @@
 import es.sanvalero.entornos.RomanNumerals;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RomanNumeralsShould {
+public class RomanNumeralsShouldTest {
     @ParameterizedTest
     @CsvSource({"1, I", "2, II", "3, III", "4, IV", "5, V", "6, VI", "7, VII", "8, VIII", "9, IX"})
     void onesToRomanNumerals(int input, String expectedOutput) {
